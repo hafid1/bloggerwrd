c75df3c31f0ef7e2a559cd480bec946c96d9121c
hafid1 <abdelhakzgani@gmail.com>
2016-08-24 15:37:05 +0200

    Create credit.js

---
+++ credit.js
@@ -0,0 +1,16 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document)['ready'](function () {
+        $('#bloggerwrd')['html']('<a http://blogger-
+
+wrd.blogspot.com" rel="dofollow" target="_blank">عالو بلوجر</a>');
+        setInterval(function () {
+                if (!$('#bloggerwrd:visible')['length']) {
+                  window['location']['href'] = 'https://blogger-
+
+wrd.blogspot.com';
+                };
+            }, 3000);
+    });
+//]]>
+</script>
