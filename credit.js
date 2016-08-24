c478346eff118aee01d7fafa590271ac6e36a592
hafid1 <abdelhakzgani@gmail.com>
2016-08-24 15:39:41 +0200

    Update credit.js

--- credit.js
+++ credit.js
@@ -1,14 +1,10 @@
 <script type='text/javascript'>
 //<![CDATA[
 $(document)['ready'](function () {
-        $('#bloggerwrd')['html']('<a http://blogger-
-
-wrd.blogspot.com" rel="dofollow" target="_blank">عالو بلوجر</a>');
+        $('#bloggerwrd')['html']('<a http://blogger-wrd.blogspot.com" rel="dofollow" target="_blank">عالو بلوجر</a>');
         setInterval(function () {
                 if (!$('#bloggerwrd:visible')['length']) {
-                  window['location']['href'] = 'https://blogger-
-
-wrd.blogspot.com';
+                  window['location']['href'] = 'http://blogger-wrd.blogspot.com';
                 };
             }, 3000);
     });
